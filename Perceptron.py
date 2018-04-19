dbfd582837365cc8946a4030fc608d231fe7e4b0
SUSHMITHA SHRIDHAR <32848089+sushmithashridhar@users.noreply.github.com>
2018-04-19 14:24:31 -0700

    Update Perceptron.py
    Weight matrix for 10 targets

--- Perceptron.py
+++ Perceptron.py
@@ -1,4 +1,3 @@
-from pandas import DataFrame, read_csv
 import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
@@ -13,4 +12,27 @@ result = np.array(pd.read_csv(file), dtype = float)
 
 result[:,1:] /= 255;
 
-print(pd.DataFrame(result))
+#print result[:,0]
+
+target = result[:,0]
+
+#print(pd.DataFrame(result))
+
+#print result
+
+#print(result.shape)
+
+#weights = np.array(np.random.uniform(-0.05, 0.05, result.shape[10][1])
+
+weights = np.random.uniform(low=-0.05, high=0.05, size=(784,10) )
+
+#result = 1*np.andom.uniform(-0.05, 0.05)
+
+
+y = np.dot(result[0,:],weights)
+
+#print weights
+#print value
+
+#print result[0]
+#print value.shape
