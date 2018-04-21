4aaa5b2a04102b5c2401f473d07b1a573d659380
SUSHMITHA SHRIDHAR <32848089+sushmithashridhar@users.noreply.github.com>
2018-04-21 01:51:52 -0700

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -2,34 +2,81 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np
  
-file = r'mnist_train.csv'
+file1 = r'mnist_train.csv'
+file2 = r'mnist_test.csv'
+
 #df = pd.read_csv(file)
 #print(pd.DataFrame(df))
 
 
-result = np.array(pd.read_csv(file), dtype = float)
+inputdata = np.array(pd.read_csv(file1), dtype = float)
+
+testdata = np.array(pd.read_csv(file2), dtype = float)
+
 #print(pd.DataFrame(result))
 
-result[:,1:] /= 255;
+inputdata[:,1:] /= 255;
 
 #print result[:,0]
 
-target = result[:,0]
+Target = inputdata[:, :1]
+
+print "Target split"
+print Target.shape
+
+inputdata = inputdata[:, 1:]
+ 
+print "inputdata split"
+print inputdata.shape
+
+one = np.ones((59999,1))
+
+value = np.hstack((one,inputdata))
+
+print "Inputdata with 1"
+print value.shape
+
 
 #print(pd.DataFrame(result))
 
 #print result
 
-#print(result.shape)
+
+
+
+
 
 #weights = np.array(np.random.uniform(-0.05, 0.05, result.shape[10][1])
 
 weights = np.random.uniform(low=-0.05, high=0.05, size=(784,10) )
 
+
+print "Weights" 
+print weights.shape
+
+#y = np.dot(target,weights)
+
+#print y.shape
+
+#print y
+
+#a = np.sum(y)
+
+#print a
+
 #result = 1*np.andom.uniform(-0.05, 0.05)
 
+#weights_transpose = weights.transpose()
 
-y = np.dot(result[0,:],weights)
+#y = np.dot(result[:,0],weights)
+
+#a = result[0,:]
+
+#print a.shape
+#
+#print y
+#
+#print y.shape
 
 #print weights
 #print value
