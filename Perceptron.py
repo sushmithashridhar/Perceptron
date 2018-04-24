155a6444e97f2fc717e26ddce77e24f783a8fe0d
SUSHMITHA SHRIDHAR <32848089+sushmithashridhar@users.noreply.github.com>
2018-04-23 21:21:24 -0700

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -3,7 +3,11 @@ import pandas as pd
 import gzip
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+learning_rate = 0.1
+#learning_rate = 0.01
+#learning_rate = 0.001
 
+##################################################################  TRAIN  ###########################################################
 
 f = gzip.open("train-images-idx3-ubyte.gz", 'rb')
 train_images = np.frombuffer(f.read(), np.uint8, offset=16)
@@ -39,25 +43,30 @@ train_y = np.zeros((60000,1))
 train_actual = np.zeros((1,60000))
 
 y_current = np.zeros((60000,10))
-accuracy = np.zeros((70))
+accuracy_train = np.zeros((70))
 for i in range(70):
     for row in range(len(train_images_matrix)):
         y_final = np.zeros([1,10],dtype=int)
         y = np.matmul(train_images_matrix[row:row+1],np.transpose(weights))
+        y = np.matmul(train_images_matrix[row:row+1],np.transpose(weights))
         maxindex = np.argmax(y, axis = 1)
         y_final[0,maxindex[0]] = 1
         delta = train_labels_final[row:row+1] - y_final
+
+        #print "train_labels_final = {}, train_images_matrix = {}".format(train_labels_final[row:row+1] ,(train_images_matrix[row:row+1]))
 
         index_y = y_final.argmax(axis = 1)
         train_y[row] = index_y
 
 
         if(delta.any(axis=1)):
-           # print("update needed")
+        # print("update needed")
             weights = weights + 0.001 * np.matmul(np.transpose(delta),train_images_matrix[row:row+1])
         else:
-         #   print("no weight update needed")
             continue
+         #print("no weight update needed")
+            
+
 #print y_final
 
     train_actual = train_y.transpose().reshape(60000,).astype(int)
@@ -67,16 +76,29 @@ for i in range(70):
     #print cfm
 
     diagonal_sum_train =  sum(np.diag(cfm_train))
-    print diagonal_sum_train
+    #print diagonal_sum_train
     accuracy_train[i] = (diagonal_sum_train/60000.00)*100
-    print accuracy_train[i]
+    #print accuracy_train[i]
 
 ############################################################### TEST ####################################################################
 
-test_predicted = test_labels.transpose().astype(int).reshape(60000,)
+
+
+f = gzip.open("t10k-images-idx3-ubyte.gz", 'rb')
+test_images = np.frombuffer(f.read(), np.uint8, offset=16)
+test_images = test_images.reshape(-1, 1, 28, 28)
+test_images = test_images.reshape(test_images.shape[0],784)
+
+f = gzip.open("t10k-labels-idx1-ubyte.gz", 'rb')
+test_labels = np.frombuffer(f.read(), np.uint8, offset=8)
+test_labels = test_labels.reshape(10000,1)
+
+
+
+test_predicted = test_labels.transpose().astype(int).reshape(10000,)
 
 #convert lables with index 1
-test_labels_final = np.zeros([60000,10],dtype=int)
+test_labels_final = np.zeros([10000,10],dtype=int)
 
 for row in range(len(test_images)):
     index = test_labels[row].astype(int)
@@ -85,18 +107,15 @@ for row in range(len(test_images)):
 #preprocessing
 test_images_matrix = np.copy(test_images)
 test_images_matrix = test_images_matrix / np.float32(255)
-bias_col = np.ones((60000,1))
+bias_col = np.ones((10000,1))
 test_images_matrix = np.append(test_images_matrix, bias_col,axis = 1)
 
+test_y = np.zeros((10000,1))
+test_actual = np.zeros((1,10000))
 
-# creating weights
-weights = np.random.uniform(-0.05, 0.05, size=(10,785))
+y_current = np.zeros((10000,10))
+accuracy_test = np.zeros((70))
 
-test_y = np.zeros((60000,1))
-test_actual = np.zeros((1,60000))
-
-y_current = np.zeros((60000,10))
-accuracy = np.zeros((70))
 for i in range(70):
     for row in range(len(test_images_matrix)):
         y_final = np.zeros([1,10],dtype=int)
@@ -108,21 +127,20 @@ for i in range(70):
         index_y = y_final.argmax(axis = 1)
         test_y[row] = index_y
 
+    test_actual = test_y.transpose().reshape(10000,).astype(int)
 
-        if(delta.any(axis=1)):
-           # print("update needed")
-            weights = weights + 0.001 * np.matmul(np.transpose(delta),test_images_matrix[row:row+1])
-        else:
-         #   print("no weight update needed")
-            continue
-
-    train_actual = train_y.transpose().reshape(60000,).astype(int)
-
-    cfm_test = confusion_matrix(train_actual, train_predicted)
+    cfm_test = confusion_matrix(test_actual, test_predicted)
 
     #print cfm
 
     diagonal_sum_test =  sum(np.diag(cfm_test))
-    print diagonal_sum_test
-    accuracy_test[i] = (diagonal_sum_test/60000.00)*100
+    #print diagonal_sum_test
+    accuracy_test[i] = (diagonal_sum_test/10000.00)*100
     print accuracy_test[i]
+
+
+
+plt.plot(accuracy_train)
+plt.plot(accuracy_test)
+plt.show()
+
