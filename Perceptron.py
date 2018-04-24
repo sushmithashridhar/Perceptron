ae64f2859e6099dbafdc0f9a451d4a1636ac392a
SUSHMITHA SHRIDHAR <32848089+sushmithashridhar@users.noreply.github.com>
2018-04-24 13:53:19 -0700

    Final plots of test and train sets

--- Perceptron.py
+++ Perceptron.py
@@ -7,7 +7,40 @@ learning_rate = 0.1
 #learning_rate = 0.01
 #learning_rate = 0.001
 
-##################################################################  TRAIN  ###########################################################
+
+########################################################## TRAIN ########################################################
+
+f = gzip.open("t10k-images-idx3-ubyte.gz", 'rb')
+test_images = np.frombuffer(f.read(), np.uint8, offset=16)
+test_images = test_images.reshape(-1, 1, 28, 28)
+test_images = test_images.reshape(test_images.shape[0],784)
+
+f = gzip.open("t10k-labels-idx1-ubyte.gz", 'rb')
+test_labels = np.frombuffer(f.read(), np.uint8, offset=8)
+test_labels = test_labels.reshape(10000,1)
+
+
+
+test_predicted = test_labels.transpose().astype(int).reshape(10000,)
+
+#convert lables with index 1
+test_labels_final = np.zeros([10000,10],dtype=int)
+
+for row in range(len(test_images)):
+    index = test_labels[row].astype(int)
+    test_labels_final[row][index[0]] += 1
+
+#preprocessing
+test_images_matrix = np.copy(test_images)
+test_images_matrix = test_images_matrix / np.float32(255)
+bias_col = np.ones((10000,1))
+test_images_matrix = np.append(test_images_matrix, bias_col,axis = 1)
+
+test_y = np.zeros((10000,1))
+test_actual = np.zeros((1,10000))
+
+y_current = np.zeros((10000,10))
+accuracy_test = np.zeros((70))
 
 f = gzip.open("train-images-idx3-ubyte.gz", 'rb')
 train_images = np.frombuffer(f.read(), np.uint8, offset=16)
@@ -44,6 +77,7 @@ train_actual = np.zeros((1,60000))
 
 y_current = np.zeros((60000,10))
 accuracy_train = np.zeros((70))
+
 for i in range(70):
     for row in range(len(train_images_matrix)):
         y_final = np.zeros([1,10],dtype=int)
@@ -61,7 +95,7 @@ for i in range(70):
 
         if(delta.any(axis=1)):
         # print("update needed")
-            weights = weights + 0.001 * np.matmul(np.transpose(delta),train_images_matrix[row:row+1])
+            weights = weights + 0.1 * np.matmul(np.transpose(delta),train_images_matrix[row:row+1])
         else:
             continue
          #print("no weight update needed")
@@ -80,43 +114,8 @@ for i in range(70):
     accuracy_train[i] = (diagonal_sum_train/60000.00)*100
     #print accuracy_train[i]
 
-############################################################### TEST ####################################################################
+####Test###
 
-
-
-f = gzip.open("t10k-images-idx3-ubyte.gz", 'rb')
-test_images = np.frombuffer(f.read(), np.uint8, offset=16)
-test_images = test_images.reshape(-1, 1, 28, 28)
-test_images = test_images.reshape(test_images.shape[0],784)
-
-f = gzip.open("t10k-labels-idx1-ubyte.gz", 'rb')
-test_labels = np.frombuffer(f.read(), np.uint8, offset=8)
-test_labels = test_labels.reshape(10000,1)
-
-
-
-test_predicted = test_labels.transpose().astype(int).reshape(10000,)
-
-#convert lables with index 1
-test_labels_final = np.zeros([10000,10],dtype=int)
-
-for row in range(len(test_images)):
-    index = test_labels[row].astype(int)
-    test_labels_final[row][index[0]] += 1
-
-#preprocessing
-test_images_matrix = np.copy(test_images)
-test_images_matrix = test_images_matrix / np.float32(255)
-bias_col = np.ones((10000,1))
-test_images_matrix = np.append(test_images_matrix, bias_col,axis = 1)
-
-test_y = np.zeros((10000,1))
-test_actual = np.zeros((1,10000))
-
-y_current = np.zeros((10000,10))
-accuracy_test = np.zeros((70))
-
-for i in range(70):
     for row in range(len(test_images_matrix)):
         y_final = np.zeros([1,10],dtype=int)
         y = np.matmul(test_images_matrix[row:row+1],np.transpose(weights))
@@ -136,11 +135,23 @@ for i in range(70):
     diagonal_sum_test =  sum(np.diag(cfm_test))
     #print diagonal_sum_test
     accuracy_test[i] = (diagonal_sum_test/10000.00)*100
-    print accuracy_test[i]
+    #print accuracy_test[i]
 
-
+    
 
 plt.plot(accuracy_train)
 plt.plot(accuracy_test)
+plt.ylabel("Accuracy in %")
+plt.xlabel("Epoch")
+
+image= "learningrate01.png"
+plt.title("learning rate 0.1")
+plt.savefig(image)
 plt.show()
+
+print "CONFUSION MATRIX OF TRAIN SET : learning rate 0.1 \n"
+print cfm_train
+
+print "CONFUSION MATRIX OF TEST SET : learning rate 0.1 \n"
+print cfm_test
 
